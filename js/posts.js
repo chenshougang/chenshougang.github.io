a01e76581c2a674d21eb0f05600d8b994f6959d7
chenshougang <1396748781@qq.com>
2025-11-02 14:14:16 +0800

    fix

--- js/posts.js
+++ js/posts.js
@@ -12,8 +12,8 @@ const posts = [
     id: 2,
     excerpt: '学习React Hooks',
     title: '学习React Hooks',
-    // date: '2023-10-01',
-    date: new Date().toISOString().split('T')[0],
+    date: '2023-10-01',
+    // date: new Date().toISOString().split('T')[0],
     tags: ['React', '前端'],
     file: '../articles/ReactHooks.md'
   },
@@ -21,8 +21,8 @@ const posts = [
     id: 3,
     excerpt: '关于效率的思考',
     title: '关于效率的思考',
-    // date: '2023-10-01',
-    date: new Date().toISOString().split('T')[0],
+    date: '2025年11月2日14:13:49',
+    // date: new Date().toISOString().split('T')[0],
     tags: ['个人成长'],
     file: '../articles/Efficiency.md'
   },
@@ -30,8 +30,8 @@ const posts = [
     id: 4,
     excerpt: '设计模式介绍和案例',
     title: '设计模式',
-    // date: '2023-10-01',
-    date: new Date().toISOString().split('T')[0],
+    date: '2025年11月2日14:14:01',
+    // date: new Date().toISOString().split('T')[0],
     tags: ['C++', '设计模式'],
     file: '../articles/DesignPattern.md'
   }
